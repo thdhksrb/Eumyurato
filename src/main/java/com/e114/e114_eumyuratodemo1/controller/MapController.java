78e29c54fcefebd07f5afa0e9f3b0ae0f7d43180
swk <swk98@naver.com>
2023-04-20 22:12:34 +0900

    비동기식 마커 띄어주기

--- src/main/java/com/e114/e114_eumyuratodemo1/controller/MapController.java
+++ src/main/java/com/e114/e114_eumyuratodemo1/controller/MapController.java
@@ -1,10 +1,16 @@
 package com.e114.e114_eumyuratodemo1.controller;
 
+import com.e114.e114_eumyuratodemo1.dto.SmallConcertDTO;
 import com.e114.e114_eumyuratodemo1.jdbc.IDAO;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 public class MapController {
@@ -28,11 +34,18 @@ public class MapController {
     }
 
     @GetMapping("/smallconcert")
-    public String smallConcert(Model model){
-
-        model.addAttribute("list",dao.viewSmallConcert());
+    public String smallConcert(){
 
         return "html/map/smallConcertMap";
+    }
+
+    @GetMapping("/smallconcert/json")
+    @ResponseBody
+    public List<SmallConcertDTO> smallConcertJson() {
+
+        List<SmallConcertDTO> list = dao.viewSmallConcert();
+
+        return list;
     }
 
 }
--- src/main/resources/static/js/map/smallConcertMap.js
+++ src/main/resources/static/js/map/smallConcertMap.js
@@ -14,69 +14,68 @@ var options = {
 };
 var map = new kakao.maps.Map(container, options);
 
-// 일반 지도와 스카이뷰로 지도 타입을 전환할 수 있는 지도타입 컨트롤을 생성합니다
+// 일반 지도와 스카이뷰로 지도 타입을 전환할 수 있는 지도타입 컨트롤을 생성
 var mapTypeControl = new kakao.maps.MapTypeControl();
 
-// 지도에 컨트롤을 추가해야 지도위에 표시됩니다
-// kakao.maps.ControlPosition은 컨트롤이 표시될 위치를 정의하는데 TOPRIGHT는 오른쪽 위를 의미합니다
+// 지도에 컨트롤을 추가해야 지도위에 표시
+// kakao.maps.ControlPosition은 컨트롤이 표시될 위치를 정의하는데 TOPRIGHT는 오른쪽 위를 의미
 map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPLEFT);
 
-// 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성합니다
+// 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성
 var zoomControl = new kakao.maps.ZoomControl();
 map.addControl(zoomControl, kakao.maps.ControlPosition.LEFT);
 
-// HTML 사이드바 요소 가져오기
-var festivals = document.getElementById('festivals');
+
+// 주소-좌표 변환 객체를 생성
+var geocoder = new kakao.maps.services.Geocoder();
 
 // 모든 마커 정보를 가져와서 출력
 var xhr = new XMLHttpRequest();
-xhr.open('GET', 'festival.json');
+xhr.open('GET', '/smallconcert/json');
 xhr.onload = function () {
     if (xhr.status === 200) {
         var data = JSON.parse(xhr.responseText);
-        for (var i = 0; i < data.records.length; i++) {
-            var record = data.records[i];
-            var marker = new kakao.maps.Marker({
-                map: map,
-                position: new kakao.maps.LatLng(record.위도, record.경도),
-                title: record.축제명
-            });
+        for (var i = 0; i < data.length; i++) {
+            var record = data[i];
 
-            // 마커에 표시할 인포윈도우를 생성합니다
-            var infowindow = new kakao.maps.InfoWindow({
-                content: data.records[i].축제명 // 인포윈도우에 표시할 내용
-            });
+            // 클로저를 사용해서 변수를 캡처
+            (function(record) {
+                // 장소명과 주소로 좌표를 검색
+                geocoder.addressSearch(record.location, function(result, status) {
+                    // 정상적으로 검색이 완료됐으면
+                    if (status === kakao.maps.services.Status.OK) {
+                        var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+                        var marker = new kakao.maps.Marker({
+                            map: map,
+                            position: coords
+                        });
 
-            // 마커에 mouseover 이벤트와 mouseout 이벤트를 등록합니다
-            // 이벤트 리스너로는 클로저를 만들어 등록합니다
-            // for문에서 클로저를 만들어 주지 않으면 마지막 마커에만 이벤트가 등록됩니다
-            kakao.maps.event.addListener(marker, 'click', clickMakerListener(map, marker, infowindow));
-            kakao.maps.event.addListener(map, 'click', clickMapListener(infowindow));
+                        // 마커에 표시할 인포윈도우를 생성
+                        var infowindow = new kakao.maps.InfoWindow({
+                            content: record.name // 인포윈도우에 표시할 내용
+                        });
 
-            var li = document.createElement('li');
-            li.innerHTML = '<h3>' + record.축제명 + '</h3>' +
-                '<p><strong>개최장소: </strong>' + record.개최장소 + '</p>' +
-                '<p><strong>축제기간: </strong>' + record.축제시작일자 + ' - ' + record.축제종료일자 + '</p>' +
-                '<p><strong>축제내용: </strong>' + record.축제내용 + '</p>';
-            festivals.appendChild(li);
+                        // 마커에 클릭 이벤트를 등록
+                        kakao.maps.event.addListener(marker, 'click', clickMakerListener(map, marker, infowindow));
+                        kakao.maps.event.addListener(map, 'click', clickMapListener(infowindow));
+                    }
+                });
+            })(record);
         }
-
-// 인포윈도우를 표시하는 클로저를 만드는 함수입니다
-        function clickMakerListener(map, marker, infowindow) {
-            return function() {
-                infowindow.open(map, marker);
-            };
-        }
-
-// 인포윈도우를 닫는 클로저를 만드는 함수입니다
-        function clickMapListener(infowindow) {
-            return function () {
-                infowindow.close();
-            };
-        }
-
-
     }
 };
-
 xhr.send();
+
+// 인포윈도우를 표시하는 클로저를 만드는 함수
+function clickMakerListener(map, marker, infowindow) {
+    return function() {
+        infowindow.open(map, marker);
+    };
+}
+
+// 인포윈도우를 닫는 클로저를 만드는 함수
+function clickMapListener(infowindow) {
+    return function () {
+        infowindow.close();
+    };
+}
