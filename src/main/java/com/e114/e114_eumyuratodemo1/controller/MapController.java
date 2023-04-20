2d61fd848167ccb644603035fbaaa783af1b737c
swk <swk98@naver.com>
2023-04-20 15:53:37 +0900

    데이터 불러오기

--- src/main/java/com/e114/e114_eumyuratodemo1/controller/MapController.java
+++ src/main/java/com/e114/e114_eumyuratodemo1/controller/MapController.java
@@ -1,10 +1,17 @@
 package com.e114.e114_eumyuratodemo1.controller;
 
+import com.e114.e114_eumyuratodemo1.jdbc.IDAO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MapController {
+
+    @Autowired
+    private IDAO dao;
+
     @GetMapping("/map")
     public String showMap() {
         return "html/map/map";
@@ -19,4 +26,13 @@ public class MapController {
     public String table() {
         return "html/pay/pay2";
     }
+
+    @GetMapping("/smallconcert")
+    public String smallConcert(Model model){
+
+        model.addAttribute("list",dao.viewSmallConcert());
+
+        return "html/map/smallConcertMap";
+    }
+
 }
--- src/main/java/com/e114/e114_eumyuratodemo1/jdbc/IDAO.java
+++ src/main/java/com/e114/e114_eumyuratodemo1/jdbc/IDAO.java
@@ -1,7 +1,11 @@
 package com.e114.e114_eumyuratodemo1.jdbc;
 
+import com.e114.e114_eumyuratodemo1.dto.SmallConcertDTO;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface IDAO {
+    public List<SmallConcertDTO> viewSmallConcert();
 }
