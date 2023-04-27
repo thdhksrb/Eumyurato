0cc72fa0f25cd393177f26ec071a1a763fcefd6a
hwewon <qkd7835@gmail.com>
2023-04-27 19:11:44 +0900

    profile_admin reservation function add html reform commit

--- src/main/java/com/e114/e114_eumyuratodemo1/controller/AdminController.java
+++ src/main/java/com/e114/e114_eumyuratodemo1/controller/AdminController.java
@@ -3,14 +3,19 @@ package com.e114.e114_eumyuratodemo1.controller;
 
 import com.e114.e114_eumyuratodemo1.dto.ArtistMemberDTO;
 import com.e114.e114_eumyuratodemo1.dto.CommonMemberDTO;
+import com.e114.e114_eumyuratodemo1.dto.EnterpriseMemberDTO;
+import com.e114.e114_eumyuratodemo1.dto.ReservationDTO;
 import com.e114.e114_eumyuratodemo1.jdbc.MemberDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -31,8 +36,12 @@ public class AdminController {
         return "redirect:/board/admin";
     }
 
-    @GetMapping("/profile/admin")
-    public String adminAccount(){
+    @GetMapping("/profile/admin/{adminId}")
+    public String adminAccount(@PathVariable("adminId") String adminId, Model model){
+        EnterpriseMemberDTO admin = memberDAO.getAdminInfoById(adminId);
+
+        model.addAttribute("admin", admin);
+
         return "html/profile/account/profile_admin_account";
     }
 
@@ -51,18 +60,21 @@ public class AdminController {
         return "html/profile/concertRegister/profile_admin_concertRegister";
     }
 
-    @GetMapping("/profile/admin/gender")
-    public String list(Model model){
+    @GetMapping("/profile/admin/total")
+    public String getCommonsAndArtistsList(Model model){
         List<ArtistMemberDTO> artists = memberDAO.getArtistMembers();
         List<CommonMemberDTO> commons = memberDAO.getCommonMembers();
+        List<EnterpriseMemberDTO> ents = memberDAO.getEntMembers();
 
         model.addAttribute("artists", artists);
         model.addAttribute("commons", commons);
+        model.addAttribute("ents", ents);
 
-        return "html/profile/gender/profile_admin_gender";
+        return "html/profile/total/profile_admin_total";
     }
-    @GetMapping("/profile/admin/gender/search")
-    public String search(@RequestParam("column") String column, @RequestParam("keyword") String keyword, Model model) {
+
+    @PostMapping("/profile/admin/total/search")
+    public String searchPostMembers(@RequestParam("column") String column, @RequestParam("keyword") String keyword, Model model) {
         Map<String, String> params = new HashMap<>();
         params.put("column", column);
         params.put("keyword", keyword);
@@ -73,8 +85,82 @@ public class AdminController {
         model.addAttribute("artists", artists);
         model.addAttribute("commons", commons);
 
-        return "html/profile/gender/profile_admin_gender";
+        return "html/profile/total/profile_admin_total";
     }
 
+    @GetMapping("/profile/admin/total/search")
+    public String searchGetMembers(@RequestParam("column") String column, @RequestParam("keyword") String keyword, Model model) {
+        Map<String, String> params = new HashMap<>();
+        params.put("column", column);
+        params.put("keyword", keyword);
+
+        List<ArtistMemberDTO> artists = memberDAO.searchArtistMembers(params);
+        List<CommonMemberDTO> commons = memberDAO.searchCommonMembers(params);
+
+        model.addAttribute("artists", artists);
+        model.addAttribute("commons", commons);
+
+        return "html/profile/total/profile_admin_total";
+    }
+
+    @PostMapping("/profile/admin/total/entSearch")
+    public String searchPostEnts(@RequestParam("column2") String column2, @RequestParam("keyword2") String keyword2, Model model) {
+        Map<String, String> params = new HashMap<>();
+        params.put("column2", column2);
+        params.put("keyword2", keyword2);
+
+        List<EnterpriseMemberDTO> ents = memberDAO.searchEntMembers(params);
+
+        model.addAttribute("ents", ents);
+
+        return "html/profile/total/profile_admin_total";
+    }
+
+    @GetMapping("/profile/admin/total/entSearch")
+    public String searchGetEnts(@RequestParam("column2") String column2, @RequestParam("keyword2") String keyword2, Model model) {
+        Map<String, String> params = new HashMap<>();
+        params.put("column2", column2);
+        params.put("keyword2", keyword2);
+
+        List<EnterpriseMemberDTO> ents = memberDAO.searchEntMembers(params);
+
+        model.addAttribute("ents", ents);
+
+        return "html/profile/total/profile_admin_total";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        session.invalidate(); // 세션 초기화
+        return "redirect:/home";
+    }
+
+    @GetMapping("/profile/admin/reservation")
+    public String reservationList(Model model) {
+        List<ReservationDTO> reservations = memberDAO.getReservationList();
+        model.addAttribute("reservations", reservations);
+
+        return "html/profile/reservation/profile_admin_reservation";
+    }
+
+    @PostMapping("/profile/admin/reservation/search")
+    public String searchReservation(@RequestParam("column") String column, @RequestParam("keyword") String keyword, Model model){
+        Map<String, String > params = new HashMap<>();
+        params.put("column", column);
+        params.put("keyword", keyword);
+
+        List<ReservationDTO> reservations = memberDAO.searchReservations(params);
+
+        model.addAttribute("reservations", reservations);
+
+        return "html/profile/reservation/profile_admin_reservation";
+    }
+
+    @GetMapping("/profile/admin/reservation/search")
+    public String searchReservations(){
+
+        return "html/profile/reservation/profile_admin_reservation";
+    }
 
 }
