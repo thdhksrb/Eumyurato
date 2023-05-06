929d6b26c03f1d45047d14243992ee648a325ab0
swk <swk98@naver.com>
2023-05-07 02:25:47 +0900

    login all complete

--- src/main/resources/static/js/detail/localDetail.js
+++ src/main/resources/static/js/detail/localDetail.js
@@ -3,6 +3,80 @@ var id = url.match(/\d+/)[0];
 var detailList = $('#detailList');
 var price = $('#price');
 
+window.onload = function() {
+    const jwtToken = window.sessionStorage.getItem("jwtToken");
+    if (jwtToken !== null) {
+        // 로그인 상태인 경우
+
+        // 토큰을 '.'으로 분리한 후, 두 번째 부분(payload)만 추출
+        const payloadBase64Url = jwtToken.split('.')[1];
+
+        // Base64Url 디코딩
+        const payloadBase64 = payloadBase64Url.replace(/-/g, '+').replace(/_/g, '/');
+        const payload = JSON.parse(atob(payloadBase64));
+        const decodedName = decodeURIComponent(escape(payload.name));
+        const userNameElem = document.getElementById("userName");
+        userNameElem.innerText = decodedName;
+
+        const mypageBtn = document.getElementById("mypageBtn");
+        mypageBtn.onclick = function (){
+
+
+            const token = sessionStorage.getItem("jwtToken");
+            const headers = {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            };
+            const options = {
+                method: 'POST',
+                headers,
+                body: JSON.stringify(payload)
+            };
+
+            fetch('/profile', options)
+                .then(response => {
+                    if (response.ok) {
+                        return response.text();
+                    } else {
+                        throw new Error('Network response was not ok');
+                    }
+                })
+                .then(data => {
+                    const URI = JSON.parse(data).URI; // 문자열을 자바스크립트 객체로 변환하여 URI 추출
+                    console.log(URI);
+                    window.location.href = URI; // 추출한 URI로 페이지 이동
+                })
+                .catch(error => console.error(error));
+        }
+
+        // 로그아웃
+        const logoutBtn = document.createElement("a");
+        logoutBtn.setAttribute("href", window.location.href);
+        logoutBtn.onclick = function() {
+            window.sessionStorage.removeItem("jwtToken");
+        };
+
+        const logoutIcon = document.createElement("img");
+        logoutIcon.setAttribute("src", "/img/logout.png");
+        logoutIcon.setAttribute("style", "height: 30px; width: 30px;");
+        logoutBtn.appendChild(logoutIcon);
+
+        const navLogin = document.getElementById("navLogin");
+        navLogin.style.display = "none";
+
+        const navLogout = document.getElementById("navLogout");
+        navLogout.style.display = "flex";
+        navLogout.querySelector("#logoutBtn").appendChild(logoutBtn);
+    } else {
+        // 로그인 상태가 아닌 경우
+        const navLogin = document.getElementById("navLogin");
+        navLogin.style.display = "flex";
+
+        const navLogout = document.getElementById("navLogout");
+        navLogout.style.display = "none";
+    }
+};
+
 $.ajax({
     url: '/local_festival/detail/'+id+'/json',
     dataType: 'json',
