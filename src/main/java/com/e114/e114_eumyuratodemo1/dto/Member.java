5714f54a1ab55a91b2b1b99ad1d46435c6a1d4b2
smc <minchae.shin1@gmail.com>
2023-05-08 02:42:49 +0900

    finPW email function final

---
+++ src/main/java/com/e114/e114_eumyuratodemo1/dto/Member.java
@@ -0,0 +1,20 @@
+package com.e114.e114_eumyuratodemo1.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Component
+public class Member {
+    private String id;
+    private String pwd;
+    private String name;
+    private String email;
+
+}
+
