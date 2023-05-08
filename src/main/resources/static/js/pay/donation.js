cc9579599f952805ee3f6bc58d89498ee7e888d8
swk <swk98@naver.com>
2023-05-08 14:38:58 +0900

    map image

--- src/main/resources/static/js/pay/donation.js
+++ src/main/resources/static/js/pay/donation.js
@@ -91,6 +91,23 @@ $.ajax({
 
         buskerInfo.append(li);
 
+        // 이미지 URL을 가져온다.
+        var imageUrl = data.image;
+
+        // 이미지 요소를 생성한다.
+        var img = document.createElement("img");
+        img.src = imageUrl;
+        img.style.objectFit = "contain";
+        img.style.width = "100%";
+        img.style.height = "100%";
+
+        // 이미지 요소를 포함할 div를 찾는다.
+        var posterContainer = document.getElementById("posterContainer");
+
+        // div에 이미지 요소를 추가한다.
+        posterContainer.appendChild(img);
+
+
         // 첫 번째 Ajax 호출이 완료된 후에 두 번째 Ajax 호출을 실행
         $('#pay').click(function (){
             localStorage.setItem('price',priceValue);
