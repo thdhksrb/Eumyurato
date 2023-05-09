54cb55387d70e0b72a5ddc5cd7049f2552efeeba
Noey <usky0604@gmail.com>
2023-05-09 22:02:17 +0900

    modify logout update

--- src/main/resources/static/js/profile/accountModify/profile_commonAccount_modfiy.js
+++ src/main/resources/static/js/profile/accountModify/profile_commonAccount_modfiy.js
@@ -279,3 +279,25 @@ function openPhonePopup() {
     // 팝업창 스타일 지정
     popup.document.body.style.backgroundColor = '#f5f5f5'; // 배경색 지정
 }
+
+// 로그아웃
+const logoutBtn = document.getElementById("logoutBtn");
+logoutBtn.setAttribute("href", "/logout");
+logoutBtn.onclick = function () {
+    fetch('/logout', { method: 'POST', credentials: 'include' })
+        .then(response => {
+            if (response.ok) {
+                // 세션 스토리지에서 토큰 제거
+                window.sessionStorage.removeItem("jwtToken");
+                console.log("로그아웃")
+                // 홈페이지로 이동
+                window.location.href = "/home";
+            } else {
+                throw new Error("로그아웃 처리에 실패하였습니다.");
+            }
+        })
+        .catch(error => {
+            console.error(error);
+            alert(error.message);
+        });
+};
