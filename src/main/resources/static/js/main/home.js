f2bb8c3d19efe80fa8234f479869b8aadf939000
smc <minchae.shin1@gmail.com>
2023-05-12 01:57:27 +0900

    html reform

--- src/main/resources/static/js/main/home.js
+++ src/main/resources/static/js/main/home.js
@@ -1,39 +1,3 @@
-/*window.onload = function() {
-    const loginUserJson = window.sessionStorage.getItem("loginUser");
-    if (loginUserJson !== null) {
-        // 로그인 상태인 경우
-        const loginUser = JSON.parse(loginUserJson);
-
-        const userNameElem = document.getElementById("userName");
-        userNameElem.innerText = loginUser.name;
-
-        const logoutBtn = document.createElement("a");
-        logoutBtn.setAttribute("href", "/logout");
-        logoutBtn.onclick = function() {
-            window.sessionStorage.removeItem("loginUser");
-        };
-
-        const logoutIcon = document.createElement("img");
-        logoutIcon.setAttribute("src", "/img/logout.png");
-        logoutIcon.setAttribute("style", "height: 30px; width: 30px;");
-        logoutBtn.appendChild(logoutIcon);
-
-        const navLogin = document.getElementById("navLogin");
-        navLogin.style.display = "none";
-
-        const navLogout = document.getElementById("navLogout");
-        navLogout.style.display = "flex";
-        navLogout.querySelector("#logoutBtn").appendChild(logoutBtn);
-    } else {
-        // 로그인 상태가 아닌 경우
-        const navLogin = document.getElementById("navLogin");
-        navLogin.style.display = "flex";
-
-        const navLogout = document.getElementById("navLogout");
-        navLogout.style.display = "none";
-    }
-};*/
-
 window.onload = function () {
     const jwtToken = window.sessionStorage.getItem("jwtToken");
     if (jwtToken !== null) {
@@ -94,7 +58,7 @@ window.onload = function () {
 
         const logoutIcon = document.createElement("img");
         logoutIcon.setAttribute("src", "/img/logout.png");
-        logoutIcon.setAttribute("style", "height: 30px; width: 30px;");
+        logoutIcon.setAttribute("style", "height: 30px; width: 30px; margin-bottom: 15px;");
         logoutBtn.appendChild(logoutIcon);
 
         const navLogin = document.getElementById("navLogin");
